187a91ec0d1afce059fc8c9ad8783207e1512195
palmerAL <PalmerAL@users.noreply.github.com>
2015-03-21 17:35:52 -0500

    better relevancy checking for pattern matches

--- spellcheck.js
+++ spellcheck.js
@@ -199,7 +199,7 @@ var commonMisspellings = [
 		});
 		var patternMatch = getLinguisticMatch(word);
 		var tempdiff = sift4(word.toUpperCase(), patternMatch.toUpperCase());
-		if ((tempdiff / patternMatch.length <= 0.3 || (tempdiff / patternMatch.length <= 0.5 && suggestionList.length < 4)) && patternMatch != word) {
+		if (tempdiff / patternMatch.length <= 0.5 && patternMatch != word) {
 			diff = tempdiff / patternMatch.length;
 			suggestionList.push(patternMatch);
 		}
