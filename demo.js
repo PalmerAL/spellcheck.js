d3dc8feddac3f90d33b88a1a84f61c4de0dc87dc
palmerAL <PalmerAL@users.noreply.github.com>
2015-06-18 11:13:39 -0500

    better accuracy, new dictionary
    Uses http://wordlist.aspell.net/ for dictionary

--- demo.js
+++ demo.js
@@ -7,3 +7,17 @@ input.onkeyup = function () {
 	output.innerHTML = spellcheck.getBestReplacement(input.value);
 	correct_display.innerHTML = spellcheck.checkSpelling(input.value);
 }
+
+var area = document.querySelector("#speller-ta-input");
+var area_output = document.querySelector("#speller-ta-output");
+
+area.onchange = function () {
+	var value = area.value;
+	var words = value.split(" ");
+	words.forEach(function (word) {
+		if (!spellcheck.checkSpelling(word)) {
+			value = value.replace(word, "<span style='color:red'>" + word + "</span>");
+		}
+	});
+	area_output.innerHTML = value;
+}
